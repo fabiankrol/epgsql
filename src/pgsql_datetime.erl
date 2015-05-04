c6fb36bbd98f9b0dfb073d1146bce415c7e10a4a
Fabian Król <fabian.krol@gmail.com>
2015-05-04 14:20:56 +0200

    Configurable datetime callback module

--- src/pgsql_datetime.erl
+++ src/pgsql_datetime.erl
@@ -3,10 +3,16 @@
 -export([mod/1]).
 -export([mod/0]).
 
+-define(APP, epgsql).
 -define(MOD, datetime_mod).
 
-mod(true) -> put(datetime_mod, pgsql_idatetime);
-mod(false) -> put(datetime_mod, pgsql_fdatetime).
+mod(true) -> mod_(integer_datetimes_mod);
+mod(false) -> mod_(non_integer_datetimes_mod).
+
+mod_(Key) ->
+    {ok, Mod} = application:get_env(?APP, Key),
+    put(?MOD, Mod),
+    ok.
 
 mod() ->
     get(?MOD).
